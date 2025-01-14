e83adf8b26f48a3968c08b494b1b47094e4b28f1
Ilan Rakhmanov <26630467+ceoguy@users.noreply.github.com>
2025-01-14 15:57:22 -0800

    Update and rename ChainGPT.sol to WhiteBridge.sol

--- WhiteBridge.sol
+++ WhiteBridge.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ChainGPT is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("ChainGPT", "CGPT") {
+contract WhiteBridge is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("WhiteBridge", "WBAI") {
         _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
 }
